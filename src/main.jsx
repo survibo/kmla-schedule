8b821219bdddb5ffb67acf7501fa9c18a34a5e76
survibo <survibo@gmail.com>
2026-04-05 23:56:36 +0900

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,8 @@
-/* global __APP_BUILD_ID__ */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register(`/sw.js?v=${__APP_BUILD_ID__}`)
-      .then((registration) => registration.update())
-      .catch(() => {})
-  })
-}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
